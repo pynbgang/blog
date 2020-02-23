cbcd2967c079a3006effc67e108e9695519235f2
ping <songpingemail@gmail.com>
2020-02-23 16:02:53 -0500

    intersection-of-two-arrays

--- leetcode/349.intersection-of-two-arrays.py
+++ leetcode/349.intersection-of-two-arrays.py
@@ -7,5 +7,5 @@
 # @lc code=start
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        
+        return list(set(nums1) & set(nums2))
 # @lc code=end
