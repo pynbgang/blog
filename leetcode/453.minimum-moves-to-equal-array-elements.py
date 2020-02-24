8e8b1c615acd63fec6a080265d6c8991d4a4ddcc
ping <songpingemail@gmail.com>
2020-02-24 11:45:24 -0500

    Minimum Moves to Equal Array Elements

---
+++ leetcode/453.minimum-moves-to-equal-array-elements.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=453 lang=python3
+#
+# [453] Minimum Moves to Equal Array Elements
+#
+
+# @lc code=start
+class Solution:
+    def minMoves(self, nums: List[int]) -> int:
+        
+# @lc code=end
