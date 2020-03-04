bcf70779d62f36dcb9ca560a6cab96d82767e234
ping <songpingemail@gmail.com>
2020-03-04 01:34:45 -0500

    [852] Peak Index in a Mountain Array

---
+++ leetcode/852.peak-index-in-a-mountain-array.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=852 lang=python3
+#
+# [852] Peak Index in a Mountain Array
+#
+
+# @lc code=start
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        
+# @lc code=end
