6198b2d110f7c7a96e0080dc608ff3e5efd00d33
ping <songpingemail@gmail.com>
2020-05-30 12:29:06 -0400

    [1341] The K Weakest Rows in a Matrix

---
+++ leetcode/1341.the-k-weakest-rows-in-a-matrix.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=1341 lang=python3
+#
+# [1341] The K Weakest Rows in a Matrix
+#
+
+# @lc code=start
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        
+# @lc code=end
